7fdf47ac3d67a288db8209a7c7f7270573235493
prachibhosekar <pbhosekar7@gmail.com>
2019-03-30 15:20:28 +0530

    dev2 first changes

--- src/hello.java
+++ src/hello.java
@@ -3,7 +3,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-        System.out.println("my first commit");
+        System.out.println("dev 2 changes");
         System.out.println("my second commit");
 	}
 
